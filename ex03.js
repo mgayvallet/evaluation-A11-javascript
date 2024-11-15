6e0b0b17993f8f80a3699a989603ba7527ed5380
qwyx <mc010116@gmail.com>
2024-11-15 14:16:44 +0100

    test exo3

--- ex03.js
+++ ex03.js
@@ -1,17 +1,17 @@
 // ex03.js
 
 function areaHardcoreCalculator(x1, y1, x2, y2) {
-    let x;
-    let y;
-    if (x1 > x2) {
-        x = x1 - x2
-    }else if (x1 < x2) {
-        x = x2 - x1
-    }else if (y1 > y2) {
-        y = y1 - y2
-    }else if (y1 < y2) {
-        y = y2 - y1
-    }
+    let x = x2 - x1
+    let y = y2 - y1
+    // if (x1 > x2) {
+    //     x = x1 - x2
+    // }else if (x1 < x2) {
+    //     x = x2 - x1
+    // }   if(y1 > y2) {
+    //     y = y1 - y2
+    // }else if (y1 < y2) {
+    //     y = y2 - y1
+    // }
 
     return x * y
 }
